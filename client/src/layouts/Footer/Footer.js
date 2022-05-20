26b42bbbda60d60bfde06d4296bd38d2520bb160
Zlatko Avramovski <zlatkoa@gmail.com>
2022-05-20 17:25:26 +0200

    Footer finished, header structure

--- client/src/layouts/Footer/Footer.js
+++ client/src/layouts/Footer/Footer.js
@@ -1,10 +1,26 @@
 import './Footer.css';
+import logo from '../../media/logo/logofooter.jpg'
+
 
 function App() {
   return (
-      <div class="footerwrapper">
-          Footer
-      </div>
+      <footer>
+        <div className="footercontainer">
+            <div className="footeritem1">
+              <img src={logo} /> 
+            </div>
+            <div className="footeritem2">
+              <a href="/"> Breakfast </a> 
+               &#9679;
+              <a href="/">Brunch</a> 
+              &#9679;
+              <a href="/">Lunch</a> 
+               &#9679;
+              <a href="/">Dinner</a> 
+             </div>
+            <div className="footeritem3"><p>Baby's Food Place <br/>copyright &copy; 2021</p> </div>
+          </div>      
+      </footer>
   );
 }
 export default App;
--- client/src/layouts/Navbar/Navbar.js
+++ client/src/layouts/Navbar/Navbar.js
@@ -1,8 +1,29 @@
+import './Navbar.css';
+import logo from '../../media/logo/logoheader.jpg'
+
+
 function App() {
-    return (
-        <div>
-            Navbar
-        </div>
-    );
+  return (
+      <nav>
+        <div className="navbarcontainer">
+            <div className="navbaritem1">
+              <img src={logo} /> 
+            </div>
+            <div className="navbaritem2">
+              <a href="/"> Breakfast </a> 
+               &#9679;
+              <a href="/">Brunch</a> 
+              &#9679;
+              <a href="/">Lunch</a> 
+               &#9679;
+              <a href="/">Dinner</a> 
+             </div>
+            <div className="navbaritem3">
+                <button>Log in</button>
+                <button>Log in</button>
+            </div>
+          </div>      
+      </nav>
+  );
 }
 export default App;
